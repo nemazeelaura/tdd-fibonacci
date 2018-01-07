5a2ee5d5bdf4fe4fd98159528cc54257e6317b3e
nemazeelaura <arvadajohnson@gmail.com>
2018-01-06 17:08:35 -0700

    uploading src and test files

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -6,3 +6,13 @@
      * @returns {Number} the nth element of the Fibonacci sequence.
      */
     // TODO: return definition of `fibonacci` function here
+  function fibonacci(index) {
+       if(index < 0) {
+       	return 0;
+       } 
+       if (index === 1 || index === 0) {
+        return 1;
+       }
+       return fibonacci(index-1) + fibonacci(index-2);
+    }
+ module.exports = fibonacci;
